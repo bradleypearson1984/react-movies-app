33b69009501d485f2d239d14ef44da0df6d50c06
brad-pearson-forever <bradleypearson1984@gmail.com>
2023-02-14 19:43:59 -0500

    adds form elements

---
+++ src/components/Form.js
@@ -0,0 +1,12 @@
+const Form = (props) => {
+    return (
+        <div>
+            <form>
+                <input type="text" />
+                <input type="submit" value="Search" />
+            </form>
+        </div>
+    )
+  };
+  
+  export default Form;
