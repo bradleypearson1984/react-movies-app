a681dacbb18c18d61984ec90c0dec939ce2015ed
brad-pearson-forever <bradleypearson1984@gmail.com>
2023-02-14 20:51:08 -0500

    makes api search functional

--- src/App.js
+++ src/App.js
@@ -4,9 +4,22 @@ import Form from "./components/Form";
 
 
 function App() {
+
+  const getMovieData = (movieTitle) => {
+    const url = 'https://www.omdbapi.com/?i=tt3896198&apikey=50532af1&t=' + movieTitle
+    fetch(url)
+    .then(response => response.json())
+    .then(data => console.log(data));
+  
+    // fetch is an http client- a function that makes an http request 
+    // fetch returns a promise 
+    // promises are objects that represent the eventual success or failure
+    // of a long running task 
+  }
+
   return (
     <div className="App">
-     <Form />
+     <Form getMovieData={getMovieData} />
       <MovieDisplay />
     </div>
   );
--- src/components/Form.js
+++ src/components/Form.js
@@ -1,8 +1,31 @@
+import { useState } from 'react';
+
 const Form = (props) => {
+    const [formState, setFormState] = useState({
+        movieTitle: ""
+    });
+
+    // an event handler is a function that gets called 
+    // after a certain event occurs
+    
+    // all event handlers can access an event object
+    const handleChange = (event) => {
+       setFormState({movieTitle: event.target.value})
+    };
+
+    const handleSubmit = (event) => {
+        event.preventDefault(); 
+        props.getMovieData(formState.movieTitle);
+    };
+
+
     return (
         <div>
-            <form>
-                <input type="text" />
+            <form onSubmit={handleSubmit} >
+                <input type="text" 
+                value={formState.movieTitle} 
+                onChange={handleChange} 
+                />
                 <input type="submit" value="Search" />
             </form>
         </div>
