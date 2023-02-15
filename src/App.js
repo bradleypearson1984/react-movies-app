5c1ea2c77bed8fa8fb712004b269dd6c7cd681a3
brad-pearson-forever <bradleypearson1984@gmail.com>
2023-02-14 21:50:03 -0500

    adds load page

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
-import './App.css';
+import { useState, useEffect } from 'react';
 import MovieDisplay from "./components/MovieDisplay";
 import Form from "./components/Form";
+import './App.css';
 
 
 function App() {
+  const [movie, setMovie] = useState(null); 
 
   const getMovieData = (movieTitle) => {
+    // console.log('calling fetch data')
     const url = 'https://www.omdbapi.com/?i=tt3896198&apikey=50532af1&t=' + movieTitle
     fetch(url)
     .then(response => response.json())
-    .then(data => console.log(data));
+    .then(data => setMovie(data));
   
     // fetch is an http client- a function that makes an http request 
     // fetch returns a promise 
@@ -17,10 +20,16 @@ function App() {
     // of a long running task 
   }
 
+  useEffect(() => {
+    //this function will run when the app first loads in the browser
+    getMovieData('Angus'); 
+  }, [] ); // this is called a dependency array 
+  // further reading: the observer pattern 
+
   return (
     <div className="App">
      <Form getMovieData={getMovieData} />
-      <MovieDisplay />
+      <MovieDisplay movie={movie}  />
     </div>
   );
 }
--- src/components/MovieDisplay.js
+++ src/components/MovieDisplay.js
@@ -1,5 +1,14 @@
 const MovieDisplay = (props) => {
-    return <h1>The MovieDisplay Component</h1>;
+    if(!props.movie) {
+        return<h2>Enter a movie to display...</h2>;
+    }
+    return (
+        <main>
+            <img src={props.movie.Poster} alt={props.movie.Title} />
+            <h2> {props.movie.Title} </h2>
+            <h3> {props.movie.Year} </h3>
+        </main>
+    )
   };
   
   
